96a60bc1d2b68301ca8cf924dfdfb7374f47496c
priteshgawade <priteshgawade11@gmail.com>
2021-11-10 11:37:16 +0530

    Dictionary using python

---
+++ dictionary.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from PyDictionary import PyDictionary
+
+dictionary=PyDictionary()
+
+
+root= Tk()
+root.title("MY Dictionary")
+root.geometry("700x400")
+def dict():
+    meaning.config(text=dictionary.meaning(word.get())['Verb'][0])
+    synonym.config(text=dictionary.synonym(word.get()))
+    antonym.config(text=dictionary.antonym(word.get()))
+
+Label(root, text="Dictionary", font=("Helvetica 20 bold"), fg="Green").pack(pady=10)
+
+frame = Frame(root)
+Label(frame, text="Type Word", font=("Helvetica 20 bold"), fg="blue",bg="yellow").pack(side=LEFT,fill="both")
+word = Entry(frame, font=("Helvetica 20 bold"))
+word.pack()
+frame.pack(pady=10)
+
+print(dictionary.meaning(word))
+
+frame1 = Frame(root)
+Label(frame1, text="Meaning:-", font=("Helvetica 20 bold"),fg="blue",bg="yellow").pack(side=LEFT,fill="x")
+meaning = Label(frame1, text="",font=("Helvetica 15 bold"),fg="white",background="purple",)
+meaning.pack(fill="x",expand=10)
+frame1.pack(pady=10)
+
+frame2 = Frame(root)
+Label(frame2, text="Synonym:-", font=("Helvetica 20 bold"),fg="blue",bg="yellow").pack(side=LEFT,fill="y")
+synonym = Label(frame2, text="",font=("Helvetica 20 bold"))
+synonym.pack()
+frame2.pack(pady=10)
+
+frame3 = Frame(root)
+Label(frame3, text="Antonym:-", font=("Helvetica 20 bold")).pack(side=LEFT)
+antonym = Label(frame3, text="",font=("Helvetica 20 bold"))
+antonym.pack(side=LEFT)
+frame3.pack(pady=10)
+
+Button(root, text="Submit", font=("Helvetica 15 bold"), command=dict).pack()
+
+root.mainloop()
